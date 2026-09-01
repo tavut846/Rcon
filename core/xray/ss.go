f2e407d87f16f2268972832e5ef480a57be5bbd4
Pandaa Xi <126928503+tavut846@users.noreply.github.com>
2026-09-01 14:56:32 +0800

    update sub

--- core/xray/ss.go
+++ core/xray/ss.go
@@ -1,4 +1,4 @@
-﻿package xray
+package xray
 
 import (
 	"encoding/base64"
@@ -60,8 +60,8 @@ func getCipherFromString(c string) shadowsocks.CipherType {
 		return shadowsocks.CipherType_AES_256_GCM
 	case "chacha20-poly1305", "aead_chacha20_poly1305", "chacha20-ietf-poly1305":
 		return shadowsocks.CipherType_CHACHA20_POLY1305
-	case "none", "plain":
-		return shadowsocks.CipherType_NONE
+	case "xchacha20-poly1305", "aead_xchacha20_poly1305", "xchacha20-ietf-poly1305":
+		return shadowsocks.CipherType_XCHACHA20_POLY1305
 	default:
 		return shadowsocks.CipherType_UNKNOWN
 	}
